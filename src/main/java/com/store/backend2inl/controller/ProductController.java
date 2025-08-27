d5b1a997ed61fffa0c2b020d502b6584bf3dd65a
Emelie572 <wahlestedt.emelie@gmail.com>
2025-08-27 17:11:47 +0200

    thymeleaf grund

--- src/main/java/com/store/backend2inl/controller/ProductController.java
+++ src/main/java/com/store/backend2inl/controller/ProductController.java
@@ -5,12 +5,16 @@ import com.store.backend2inl.repository.ProductRepository;
 import com.store.backend2inl.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 //TODO: Gör om till Controller för Thymeleaf
+
+//@Controller
 @RestController
 public class ProductController {
 
@@ -21,7 +25,34 @@ public class ProductController {
     public ProductController(ProductRepository productRepository, ApiService apiService) {
         this.productRepository = productRepository;
         this.apiService = apiService;
+
     }
+
+
+    @GetMapping("/products")
+    public String showProducts(Model model) {
+        try {
+            apiService.fetchAndSaveProducts();
+            List<Product> products = productRepository.findAll();
+
+            model.addAttribute("products", products);
+            model.addAttribute("pageTitle", "Produkter");
+            model.addAttribute("productPrice", "Pris");
+            model.addAttribute("productDescription", "Beskrivning");
+
+            return "products";
+
+        } catch (Exception e) {
+            System.err.println("Fel vid hämtning av produkter: " + e.getMessage());
+            model.addAttribute("errorMessage", "Kunde inte hämta produkter just nu.");
+        }
+        return "products";
+    }
+
+
+
+
+    //RESTAPI
 
     @GetMapping("/fetch/all")
     public ResponseEntity<List<Product>> fetchProducts() throws Exception {
