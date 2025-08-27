cb39035d9b5f86ba617f3fc793c4c1fd13317ce9
Kim Zugic <kim.zugic@live.com>
2025-08-27 20:28:39 +0200

    småfix

--- src/main/java/com/store/backend2inl/service/ApiService.java
+++ src/main/java/com/store/backend2inl/service/ApiService.java
@@ -1,16 +1,11 @@
 package com.store.backend2inl.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.store.backend2inl.model.Product;
 import com.store.backend2inl.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Component
 public class ApiService {
@@ -35,6 +30,7 @@ public class ApiService {
                 }
             }
         } catch (Exception e) {
+            System.out.println("There was a problem saving the products");
             e.printStackTrace();
         }
     }
