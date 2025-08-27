9a6af840ec50806d0dbb7684d3dc74d23ea84467
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-08-27 14:19:46 +0200

    Initial commit

---
+++ src/main/java/com/store/backend2inl/Backend2InlApplication.java
@@ -0,0 +1,13 @@
+package com.store.backend2inl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2InlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend2InlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/backend2inl/Backend2InlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.backend2inl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2InlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
