76109b2b11022513ce0562e058a24e0866a943ef
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-08-30 13:12:47 +0200

    fixade testen

--- src/test/java/com/store/backend2inl/IntegrationTestIT.java
+++ src/test/java/com/store/backend2inl/IntegrationTestIT.java
@@ -6,21 +6,61 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntegrationTestIT {
 
-    @Test
-    void shouldReturnJSONFromApi() {
-        String url = "https://fakestoreapi.com/products";
-        RestTemplate restTemplate = new RestTemplate();
-        String json = restTemplate.getForObject(url, String.class);
+    String url = "https://fakestoreapi.com/products";
+    RestTemplate restTemplate = new RestTemplate();
+    String json = restTemplate.getForObject(url, String.class);
 
-        System.out.println("Testing if it contains all attributes");
+    @Test
+    void shouldReturnId() {
+        System.out.println("Testing if it contains id");
         assertTrue(json.contains("id"));
+    }
+
+    @Test
+    void shouldReturnTitle() {
+        System.out.println("Testing if it contains title");
         assertTrue(json.contains("title"));
+    }
+
+    @Test
+    void shouldReturnPrice() {
+        System.out.println("Testing if it contains price");
         assertTrue(json.contains("price"));
+    }
+
+    @Test
+    void shouldReturnDescription() {
+        System.out.println("Testing if it contains description");
         assertTrue(json.contains("description"));
+    }
+
+    @Test
+    void shouldReturnCategory() {
+        System.out.println("Testing if it contains category");
         assertTrue(json.contains("category"));
+    }
+
+    @Test
+    void shouldReturnImage() {
+        System.out.println("Testing if it contains image");
         assertTrue(json.contains("image"));
+    }
+
+    @Test
+    void shouldReturnRating() {
+        System.out.println("Testing if it contains rating");
         assertTrue(json.contains("rating"));
+    }
+
+    @Test
+    void shouldReturnRate() {
+        System.out.println("Testing if it contains rate");
         assertTrue(json.contains("rate"));
+    }
+
+    @Test
+    void shouldReturnCount() {
+        System.out.println("Testing if it contains count");
         assertTrue(json.contains("count"));
     }
 }
