e4d54f68e3e14ed6507b6f0ad08cf9757f2f6096
Kim Zugic <kim.zugic@live.com>
2025-08-31 20:29:22 +0200

    login form added

--- src/main/java/com/store/backend2inl/controller/UserController.java
+++ src/main/java/com/store/backend2inl/controller/UserController.java
@@ -1,4 +1,70 @@
 package com.store.backend2inl.controller;
 
+
+import com.store.backend2inl.model.User;
+import com.store.backend2inl.repository.UserRepo;
+import com.store.backend2inl.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("user" , new User());
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("user" , new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String username,
+                               @RequestParam String password,
+                               Model model){
+        if(userRepo.findUserByName(username).isPresent()){
+            model.addAttribute("error" , "That username is already taken!");
+            return "register";
+        }
+        User user = new User();
+        user.setUsername(username);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        model.addAttribute("user" , user);
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        Model model){
+        return userService.findUserByName(username).
+                filter(hashed -> new BCryptPasswordEncoder().matches(password, hashed)).
+                map(found -> {
+                    model.addAttribute("username" , username);
+                    return "products";
+                }).orElseGet(() -> {
+                    User failedUser = new User();
+                    failedUser.setUsername(username);
+                    model.addAttribute("user" , failedUser);
+                    model.addAttribute("error" , "Incorrect username or password!");
+                    return "login";
+                });
+
+    }
 }
