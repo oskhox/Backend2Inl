0137ff08153da61e6bc5f64fefe9da0094069dc7
Kim Zugic <kim.zugic@live.com>
2025-08-31 20:10:18 +0200

    check if user exists

--- src/main/java/com/store/backend2inl/model/User.java
+++ src/main/java/com/store/backend2inl/model/User.java
@@ -1,4 +1,23 @@
 package com.store.backend2inl.model;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Getter
+    private String username;
+
+    @Setter
+    @Getter
+    private String password;
+
 }
--- src/main/java/com/store/backend2inl/service/UserService.java
+++ src/main/java/com/store/backend2inl/service/UserService.java
@@ -1,4 +1,21 @@
 package com.store.backend2inl.service;
 
+
+import com.store.backend2inl.model.User;
+import com.store.backend2inl.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepo userrepo;
+
+    public Optional<String> findUserByName(String username){
+
+        return userrepo.findUserByName(username).map(User::getUsername);
+    }
 }
