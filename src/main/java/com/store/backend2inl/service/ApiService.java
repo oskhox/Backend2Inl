c7d4509f1960ca7cd5d27de39c24361f28875723
Kim Zugic <kim.zugic@live.com>
2025-09-03 21:43:57 +0200

    fixed products saving multiple times

--- src/main/java/com/store/backend2inl/service/ApiService.java
+++ src/main/java/com/store/backend2inl/service/ApiService.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -37,7 +38,11 @@ public class ApiService {
                     product.setDescription(p.getDescription());
                     product.setCategory(p.getCategory());
                     product.setImage(p.getImage());
-                    productRepository.save(product);
+
+                    if(!productRepository.existsById(p.getId())) {
+                        productRepository.save(product);
+                    }
+
                     //System.out.println("Saved product " + p.getTitle());
                 }
             }
