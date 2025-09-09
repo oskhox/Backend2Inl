89c175e9646eebef32c9b889c9013e74fec9fbf6
Emelie572 <wahlestedt.emelie@gmail.com>
2025-09-09 22:35:16 +0200

    fix orderconfirm

--- src/main/java/com/store/backend2inl/controller/UserController.java
+++ src/main/java/com/store/backend2inl/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -58,58 +57,4 @@ public class UserController {
         return "customer";
     }
 
-
-
-
-
-    /*
-    @GetMapping("/login")
-    public String login(Model model){
-        model.addAttribute("user" , new User());
-        return "login";
-    }
-*/
-/*
-    @PostMapping("/register")
-    public String registerUser(@RequestParam String username,
-                               @RequestParam String password,
-                               Model model){
-        if(userRepo.findByUsername(username).isPresent()){
-            model.addAttribute("error" , "That username is already taken!");
-            return "register";
-        }
-        User user = new User();
-        user.setUsername(username);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        user.setPassword(passwordEncoder.encode(password));
-        userRepo.save(user);
-
-        model.addAttribute("success" , "User registered successfully!");
-        model.addAttribute("user" , user);
-        model.addAttribute("password" , password);
-        return "login";
-    }
-*/
-
-    /*
-    @PostMapping("/login")
-    public String login(@RequestParam String username,
-                        @RequestParam String password,
-                        Model model){
-        return userService.findUserByUsername(username).
-                filter(user -> new BCryptPasswordEncoder().matches(password, user.getPassword())).
-                map(user -> {
-                    model.addAttribute("pageTitle" , "Alla produkter");
-                    model.addAttribute("products", productService.getAllProducts());
-                    return "products";
-                }).orElseGet(() -> {
-                    model.addAttribute("user" , new User());
-                    model.addAttribute("errorMessage" , "Incorrect username or password!");
-                    return "login";
-                });
-
-    }
-
-
-     */
 }
